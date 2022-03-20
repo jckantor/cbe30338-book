411ef14beef033d9133a56bee43616e67a08a5b4
Jeffrey Kantor <kantor.1@nd.edu>
2022-03-20 18:28:03 -0400

    fix

--- _build/jupyter_execute/tclab/05.00-State-Estimation.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # State Estimation
-# 
-# 
-# ## Reference materials
-# 
-# * Wang, Shuo. "Kalman Filter in a Nutshell." *Towards Data Science*, https://towardsdatascience.com/kalman-filter-in-a-nutshell-e66154a06862. Accessed 6 December 2020.
